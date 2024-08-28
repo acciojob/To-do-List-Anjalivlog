b9439538e356ce9afccf7b99beb5fdf5dbc04e9c
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-08-28 18:50:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+document.getElementById('addTodoBtn').addEventListener('click', function() {
+	const input = document.getElementById('newTodoInput');
+	const todoText = input.value.trim();
+	if(todoText === '') {
+		alert('Please enter a todo item.');
+        return;
+	}
+	const todoList = document.getElementById('todoList');
+	const newTodo = document.createElement('li');
+	newTodo.textContent = todoText;
 
+	newTodo.addEventListener('click', function () {
+		input.value = '';
+	})
+	todoList.appendChild(newTodo);
+    input.value = '';
+})
